a3d206e05db11c940f723980dedeb4f15d8a2f44
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-11-18 17:30:45 +0530

    Update my_first_script.sh

--- my_first_script.sh
+++ my_first_script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Author: Ashish Shejale
+# Date Created: 18-Nov-2021
+# Date Modified: 18-Nov-2021
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# my_first_script
+
 echo "This is my first script"
 exit 0
